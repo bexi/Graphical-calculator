d4a59f08243ca83cafe92b428dc3edeae19b03ec
bexi <trreitmaier@msn.com>
2016-12-05 18:25:07 +0100

    added impl for Cos, Sin and X in showExpr

--- Expr.hs
+++ Expr.hs
@@ -5,13 +5,18 @@ import Test.QuickCheck
 
 -------------------------------------------------------------------------
 
--- test data
+-- test data for Add and Mul
 expr1 = Add (Num 5) (Num 3)
 expr2 = Mul expr1 expr1
 expr3 = Add expr2 (Num 6)
 expr4 = Mul (Num 5) (Num 3)
 expr5 = Add expr2 expr4
 expr6 = Add expr4 expr4
+-- test data for Sin and Cos
+expr7 = Sin (Num 5)
+expr8 = Sin expr1
+expr9 = Cos expr6
+expr10 = Cos (Add X expr1)
 
 instance Arbitrary Expr where
   arbitrary = sized arbExpr
@@ -26,15 +31,25 @@ data Expr =   Num Double
 
 -- converts any expression to string
 showExpr :: Expr -> String
+showExpr (X)                      = "x"
 showExpr (Num n) | isInt n        = show (round n)
 showExpr (Num n) | not (isInt n)  = show n
 showExpr (Add a b)                = showExpr a ++ "+" ++ showExpr b
 showExpr (Mul a b)                = showFact a ++ "*" ++ showFact b
+showExpr (Sin a)                  = "sin " ++ showTrig a
+showExpr (Cos a)                  = "cos " ++ showTrig a
 
 showFact :: Expr -> String
 showFact (Num n)    = showExpr (Num n)
 showFact (Add a b)  = "(" ++ showExpr (Add a b) ++ ")"
 showFact (Mul a b)  = showExpr (Mul a b)
+
+showTrig :: Expr -> String
+showTrig (X)        = showExpr (X)
+showTrig (Num n)    = showExpr (Num n)
+showTrig (Add a b)  = "(" ++ showExpr (Add a b) ++ ")"
+showTrig (Mul a b)  = "(" ++ showExpr (Mul a b) ++ ")"
+
 
 isInt :: Double -> Bool
 isInt x = x == fromInteger (round x)
