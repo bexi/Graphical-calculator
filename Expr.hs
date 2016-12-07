a0fe74aa1e0ae8cc51de15898659888d9ea0aa34
bexi <trreitmaier@msn.com>
2016-12-07 14:52:35 +0100

    changed the data structure for Expr

--- Expr.hs
+++ Expr.hs
@@ -1,55 +1,55 @@
 
-module Expr where
+module Test where
 
 import Test.QuickCheck
 
 -------------------------------------------------------------------------
-
--- test data for Add and Mul
-expr1 = Add (Num 5) (Num 3)  -- 8
-expr2 = Mul expr1 expr1     -- 64
-expr3 = Add expr2 (Num 6)   -- 70
-expr4 = Mul (Num 5) (Num 3) -- 15
-expr5 = Add expr2 expr4     -- 79
-expr6 = Add expr4 expr4     -- 30
--- test data for Sin and Cos
-expr7 = Sin (Num 5)
-expr8 = Sin expr1
-expr9 = Cos expr6
-expr10 = Cos (Add X expr1)  -- x=5 --> 0.9 
 
 instance Arbitrary Expr where
   arbitrary = sized arbExpr
 
 -- represents expressions
 data Expr =   Num Double
-            | Add Expr Expr
-            | Mul Expr Expr
-            | Sin Expr
-            | Cos Expr
-            | X
+            | Operator Op Expr Expr
+            | Function Fu Expr
+            | X
+
+data Op = Op {func :: Double -> Double -> Double,
+              name :: String,
+              prio :: Int}
+
+data Fu = Fu {funcF :: Double -> Double,
+              nameF :: String,
+              prioF :: Int}
+
+--operations
+mul  = Op {func=(*), name="*", prio=2}
+plus = Op {func=(+), name="+", prio=1}
+--functions
+cos' = Fu {funcF=(cos), nameF="cos ", prioF=3}
+sin' = Fu {funcF=(sin), nameF="sin ", prioF=3}
+--test data
+expr1 = Operator plus (Num 2) (Num 8) --2+8
+expr2 = Operator mul (Num 1) (Num 7)  --1*7
+expr3 = Operator plus expr1 expr2     --2+8+1*7
+expr4 = Operator mul expr1 expr2      --(2+8)*(1*7) obs
+expr5 = Function cos' (Num 5)         --cos (5)
+expr6 = Function sin' expr3           --sin (2+8+1*7)
 
 -- converts any expression to string
 showExpr :: Expr -> String
 showExpr (X)                      = "x"
 showExpr (Num n) | isInt n        = show (round n)
 showExpr (Num n) | not (isInt n)  = show n
-showExpr (Add a b)                = showExpr a ++ "+" ++ showExpr b
-showExpr (Mul a b)                = showFact a ++ "*" ++ showFact b
-showExpr (Sin a)                  = "sin " ++ showTrig a
-showExpr (Cos a)                  = "cos " ++ showTrig a
+showExpr (Operator o e1 e2) | (prio o == 1) = showExpr e1 ++ name o ++ showExpr e2
+showExpr (Operator o e1 e2) | (prio o == 2) = showExpr' e1 ++ name o ++ showExpr' e2
+showExpr (Function o e)                     = nameF o ++ "(" ++ showExpr e ++ ")"
 
-showFact :: Expr -> String
-showFact (Num n)    = showExpr (Num n)
-showFact (Add a b)  = "(" ++ showExpr (Add a b) ++ ")"
-showFact (Mul a b)  = showExpr (Mul a b)
-
-showTrig :: Expr -> String
-showTrig (X)        = showExpr (X)
-showTrig (Num n)    = showExpr (Num n)
-showTrig (Add a b)  = "(" ++ showExpr (Add a b) ++ ")"
-showTrig (Mul a b)  = "(" ++ showExpr (Mul a b) ++ ")"
-
+showExpr' :: Expr -> String
+showExpr' (Num n) = showExpr (Num n)
+showExpr' (Operator o e1 e2) | (prio o == 1) = "(" ++ showExpr e1 ++ name o ++ showExpr e2 ++ ")"
+showExpr' (Operator o e1 e2) | (prio o == 2) = "(" ++ showExpr' e1 ++ name o ++ showExpr' e2 ++ ")"
+showExpr' (Function o e)                     = showExpr (Function o e)
 
 isInt :: Double -> Bool
 isInt x = x == fromInteger (round x)
@@ -57,12 +57,10 @@ isInt x = x == fromInteger (round x)
 -- calculates the value of the expression,
 --the second parameter is the wanted value of X
 eval :: Expr -> Double -> Double
-eval (X)       x = x
-eval (Num n)   x = n
-eval (Add a b) x = (eval a x) + (eval b x)
-eval (Mul a b) x = (eval a x) * (eval b x)
-eval (Sin a)   x = sin (eval a x)
-eval (Cos a)   x = cos (eval a x)
+eval X x                  = x
+eval (Num n) x            = n
+eval (Operator o e1 e2) x = (func o) (eval e1 x) (eval e2 x)
+eval (Function o e) x     = (funcF o) (eval e x)
 
 -- tries to interpret the string as an expression,
 -- and returns Just of that expression if it succeeds.
